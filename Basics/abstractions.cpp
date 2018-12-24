c10845585a4afe1e02a3b817d548bfc316ff8d56
Arun Ravindran <arun_a_r@rocketmail.com>
2018-12-24 13:02:00 -0500

    Done class hiearchies

--- Basics/abstractions.cpp
+++ Basics/abstractions.cpp
@@ -195,3 +195,154 @@ void h()
 // Caveat: objects now must be maipulated through references or pointers.
 // Compiler converts name of virtual function into an index into a table of functions
 // This table is called virtual function table (vtbl). Each class maintains its vtbl
+
+//~~~~~~~Class Hiearchies~~~~~~~~`
+
+// A class hierarchy is a set of classes ordered in a lattice created by derivation 
+// We use class hierarchies to represent concepts that have an "is a" relationship
+// A fire engine is a kind of a truck or A smiley face is a kind of a circle which is a kind of a shape
+
+// Abstract class shape
+class Shape {
+public:
+    virtual Point center() const =0;
+    virtual void move(Point to) =0; // pure virtual
+    virtual void draw() const = 0;
+    virtual void rotate(int angle) = 0; // draw on current "Canvas"
+    virtual  ~Shape() {} // destructor
+};
+
+// Using shapes in a Canvas application
+void rotate_all(vector<Shape∗>& v, int angle) // rotate v’s elements by angle degrees
+{
+    for (auto p : v)
+        p−>rotate(angle);
+}
+
+// Define a concrete shape Circle
+class Circle : public Shape { //Subclass Circle from Shape
+private:
+    Point x; // center
+    int r; // radius
+public:
+    Circle(Point p, int rr); // constructor
+    Point center() const { return x; }
+    void move(Point to) { x=to; }
+    void draw() const;
+    void rotate(int) {}
+
+};
+
+// Define a concerte shape Smiley derived from Circle
+class Smiley : public Circle { // use the circle as the base for a face
+private:
+    vector<Shape∗> eyes;
+    Shape∗ mouth;
+public:
+    Smiley(Point p, int r) : Circle{p,r}, mouth{nullptr} { } //Initialize super class constructor
+    ~Smiley()
+    {
+        delete mouth;
+        for (auto p : eyes) delete p;
+    }
+    void move(Point to);
+    void draw() const;
+    void rotate(int);
+    void add_eye(Shape∗ s) { eyes.push_back(s); }
+    void set_mouth(Shape∗ s);
+    virtual void wink(int i);   // wink eye number i
+    // ...
+};
+
+void Smiley::draw()
+{
+    Circle::draw();
+    for (auto p : eyes)
+        p−>draw();
+    mouth−>draw();
+}
+
+// Note that subclasses maybe used as type superclasses (Smiley is a Shape)
+// So superclass destructors have to be virtual to call the appropriaate subclass destructor
+
+// Two kinds of inhertiances - Interface inheritance. For example, inheriting from Shape
+// Immplmentation inheritance. For example, inheriting constructor from Circle
+
+// More examples of how class hiearchies are used
+// Read Shapes
+enum class Kind { circle, triangle , smiley };
+Shape∗ read_shape(istream& is) // read shape descriptions from input stream is
+{
+    // ... read shape header from is and find its Kind k ...
+    switch (k) {
+    case Kind::circle:
+        // read     circle data {Point,int} into p and r
+        return new Circle{p,r};
+    case Kind::triangle:
+        // read triangle data {Point,Point,Point} into p1, p2, and p3
+    return new Triangle{p1,p2,p3};
+    case Kind::smiley:
+        // read smiley data {Point,int,Shape,Shape,Shape} into p, r, e1 ,e2, and m
+        Smiley∗ ps = new Smiley{p,r};
+        ps−>add_eye(e1);
+        ps−>add_eye(e2);
+        ps−>set_mouth(m);
+    return ps;
+    }
+}
+
+// More Shape operations
+void user()
+{
+    std::vector<Shape∗> v;
+    while (cin)
+        v.push_back(read_shape(cin));
+    draw_all(v);    // call draw() for each element
+    rotate_all(v,45); // call rotate(45) for each element
+    for (auto p : v) delete p;  // remember to delete elements
+}
+
+// User must delete all pointers 
+// Use unique pointers instead from the standard library
+// Rewrite of read_shape() with unique_ptr
+unique_ptr<Shape> read_shape(istream& is) // read shape descriptions from input stream is
+{
+    // read shape header from is and find its Kind k
+    switch (k) {
+        case Kind::circle:
+    // read circle data {Point,int} into p and r
+        return unique_ptr<Shape>{new Circle{p,r}};
+    // ...
+
+    }
+void user()
+{
+    vector<unique_ptr<Shape>> v;
+    while (cin)
+        v.push_back(read_shape(cin));
+    draw_all(v);    // call draw() for each element
+    rotate_all(v,45);   // call rotate(45) for each element
+} // all Shapes implicitly destroyed when unique_ptr goes out of scope
+
+// Note: all functions (for example rotate_all()) has to use unique_ptr as well
+
+
+//~~~~~~~~Copy and Move~~~~
+
+// By default, objects can be copied
+// This is true for objects of user-defined types as well as for built-in types.
+// The default meaning of copy is memberwise copy: copy each member
+// May not be appropriate for all object - for example, vector class define earlier
+
+// Eexample - for the complex class defined earlier
+
+void test(complex z1)
+{
+    complex z2 {z1}; // copy initialization
+    complex z3; // copy assignment
+    z3 = z2;
+    // ...
+}
+
+// z1 , z2 , and z3 have the same value because both the assignment and the initialization copied both members.
+
