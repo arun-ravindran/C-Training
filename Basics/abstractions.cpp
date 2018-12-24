349e69ea899b6203a9f47d0fdb1ec6f1a5022398
Arun Ravindran <arun_a_r@rocketmail.com>
2018-12-24 12:21:23 -0500

    Done abstract classes

--- Basics/abstractions.cpp
+++ Basics/abstractions.cpp
@@ -96,3 +96,102 @@ void fct(int n)
 // ... use v ..
 
 } // v is destroyed here
+
+// RAII -  Resource Allocation is Initialization. Client code avoid allocating and deallocatin resources
+
+// Additional ways to initialize container
+
+class Vector {
+public:
+    Vector(std::initializer_list<double>); // initialize with a list
+    // ...
+
+};
+
+Vector::Vector(std::initializer_list<double> lst) // initialize with a list
+    :elem{new double[lst.size()]}, sz{lst.size()}
+{
+    copy(lst.begin(),lst.end(),elem); // copy from lst into elem
+
+}
+
+//Usage
+Vector v1 = {1,2,3,4,5}; // v1 has 5 elements
+Vector v2 = {1.23, 3.45, 6.7, 8}; // v2 has 4 elements
+
+
+//~~~~~Abstract types~~~~~~~
+// Abstract types insulates the user from immplementation details
+// Objects have to be allocated on the free store, and accessed through pointers or references
+
+// Define an interface for an abstract class Container
+class Container {
+public:
+    virtual double& operator[](int) = 0; // pure virtual function
+    virtual int size() const = 0; // const member function
+    virtual  ~Container() {} // destructor
+};
+
+// virtual means may be redefined later in a class derived from this one - inhertiance and polymorphism
+// pure virutal (=0) means that the derived class must define the function
+// no constructor since nothing to initialize
+// destructor is virtual - will call derived class destructors
+
+// Use of Container abstract class. No idea what the actual container will be! 
+void use(Container& c)
+{
+    const int sz = c.size();
+    for (int i=0; i!=sz; ++i)
+        cout << c[i] << '\n';
+}
+
+// Derive a concrete Vector_container subclass from the superclass (also called base class) Container
+class Vector_container : public Container { // Vector_container implements Container
+    Vector v;
+public:
+    Vector_container(initializer_list<double> il) : v{il} { }
+    ~Vector_container() {} // Call Vectors destructor
+    double& operator[](int i) { return v[i]; } // Overrides superclass implementation - polymorphism
+    int size() const { return v.size(); } // Overrides superclass implementation
+};
+
+// Use case
+void g()
+{
+    Vector_container vc {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; 
+    use(vc);
+}
+
+// Yet another subclass from Vector_container super class
+class List_container : public Container { // List_container implements Container
+    std::list<double> ld; // (standard-library) linked list of doubles 
+
+public:
+    List_container() { } // empty List
+    List_container(initializer_list<double> il) : ld{il} { }
+    ~List_container() {}
+    double& operator[](int i);
+    int size() const { return ld.size();
+};
+double& List_container::operator[](int i)
+{
+    for (auto& x : ld) {
+        if (i==0) return x;
+    −−i;
+    }
+    throw out_of_rang e("List container");
+}
+
+//Use case
+void h()
+{
+    List_container lc = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    use(lc);
+}
+
+// Important: use(Container& ) has on idea if the argument is Vector_container or List_container
+// It only knows the inteface defined by Container
+// Use_container need not be recompiled if derived classes change.
+// Caveat: objects now must be maipulated through references or pointers.
+// Compiler converts name of virtual function into an index into a table of functions
+// This table is called virtual function table (vtbl). Each class maintains its vtbl
