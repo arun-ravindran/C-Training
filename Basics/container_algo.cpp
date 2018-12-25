8df41ae8ec5684524c90ba7b309b17dbef9ceeea
Arun Ravindran <arun_a_r@rocketmail.com>
2018-12-24 22:46:24 -0500

    Done with vectors

--- Basics/container_algo.cpp
+++ Basics/container_algo.cpp
@@ -94,3 +94,109 @@ void hello_line()
 }
 
 // I/O of user defined types
+struct Entry {
+    string name;
+    int number;
+};
+
+// Output
+ostream& operator<<(ostream& os, const Entry& e)
+{
+    return os << "{\"" << e.name << "\", " << e.number << "}";
+}
+
+// Input : More complicated because of formatting and error handling
+// Note: is >> c produces a bool indicating success at reading is into c
+
+istream& operator>>(istream& is, Entry& e)
+// read { "name" , number } pair. Note: for matted with { " " , and }
+{
+    char c, c2;
+    if (is>>c && c=='{' && is>>c2 && c2=='"') { // star t with a { "
+        string name;    // the default value of a string is the empty string: ""
+        while (is.get(c) && c!='"') // anything before a " is part of the name
+            name+=c;
+
+        if (is>>c && c==',') {
+            int number = 0;
+            if (is>>number>>c && c=='}') { // read the number and a }
+                e = {name ,number};`// assign to the entry
+                return is;
+            }
+        }
+    }
+    is.setf(ios_base::failbit); // register the failure in the stream
+    return is;
+}
+
+// Usage
+for (Entry ee; cin>>ee; ) // read from cin into ee
+    cout << ee << '\n'; // write ee to cout
+
+
+
+//~~~~~Containers - vectors~~~~~~~~~~
+
+// Class with the main purpose of holding objects is commonly called a container
+
+// The most useful standard-library container is vector 
+// A vector is a sequence of elements of a given type. The elements are stored contiguously in memory
+
+// Example use 
+vector<Entry> phone_book = { // Entry defined above
+    {"David Hume",123456},
+    {"Karl Popper",234567},
+    {"Bertrand Ar thur William Russell",345678}
+};
+
+// Element access through subscripting
+void print_book(const vector<Entry>& book)
+{
+    for (int i = 0; i!=book.size(); ++i)
+        cout << book[i] << '\n';
+}
+
+// Range loop
+void print_book(const vector<Entry>& book)
+{
+    for (const auto& x : book)
+        cout << x << '\n';
+}
+
+// Initialization
+vector<int> v1 = {1, 2, 3, 4}; // size is 4
+vector<string> v2; // size is 0
+vector<Shape∗> v3(23); // size is 23; initial element value: nullptr
+vector<double> v4(32,9.9); // size is 32; initial element value: 9.9
+
+
+// Adding element. Automatic size increase
+void input()
+{
+    for (Entr y e; cin>>e;)
+        phone_book.push_back(e);
+}
+
+// Copying vectors
+vector<Entry> book2 = phone_book; //Note: Use pointers if copying is expensive
+
+// Templating vectors
+// vector<T> where T can be built in types, user defined types, pointers 
+
+// Note: subscript operator [] does not do range checking. Use at() operator instead
+// at() throws an exception of type out_of_range
+
+int main()
+    try {
+        // your code
+    }
+    catch (out_of_rang e) {
+        cerr << "range error\n";
+    }
+    catch (...) { //Default exceptioin handler
+        cerr << "unknown exception thrown\n";
+    }
+}
+
+
+//~~~~~Containers - lists~~~~~~~~~~
