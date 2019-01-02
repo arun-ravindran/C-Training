805ed473723cd13e2d6c4693c2d6d93522e0038c
Arun Ravindran <arun_a_r@rocketmail.com>
2019-01-02 18:13:25 -0500

    No adapter example done

---
+++ DesignPatterns/Adpater/no_adapter.cpp
@@ -0,0 +1,89 @@
+/* Testing devices with two different interfaces without applying the Adapter pattern. No code reuse */
+
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class DeviceInterface {
+public:
+    virtual void on() = 0;
+    virtual void applyInput() = 0;
+    virtual void measureOutput() = 0;
+    virtual void off() = 0;
+};
+
+class Device : public DeviceInterface {
+public:
+    void on() override {cout << "Turn on device" << endl;} 
+    void applyInput() override{cout << "Apply input to device" << endl;}
+    void measureOutput() override {cout << "Measure output from device" << endl;}
+    void off() override {cout << "Turn off device" << endl;}
+};
+
+class TestDevice {
+protected:
+    shared_ptr<DeviceInterface> dev;
+public:
+    TestDevice(shared_ptr<DeviceInterface> d) : dev(d) {} 
+    void applyTest();
+};
+
+void TestDevice::applyTest()
+{
+    dev->on();
+    dev->applyInput();
+    dev->measureOutput();
+    dev->off();
+}
+
+class NewDeviceInterface {
+public:
+    virtual void on() = 0;
+    virtual void applyLoInput() = 0;
+    virtual void applyHiInput() = 0;
+    virtual void measureOutput() = 0;
+    virtual void off() = 0;
+};
+
+class NewDevice : public NewDeviceInterface {
+public:
+    void on() override {cout << "Turn on device" << endl;} 
+    void applyLoInput() override {cout << "Apply low input to device" << endl;}
+    void applyHiInput() override {cout << "Apply high input to device" << endl;}
+    void measureOutput() override {cout << "Measure output from device" << endl;}
+    void off() override {cout << "Turn off device" << endl;}
+};
+
+
+class TestNewDevice {
+protected:
+    shared_ptr<NewDeviceInterface> dev;
+public:
+    TestNewDevice(shared_ptr<NewDeviceInterface> d) : dev(d) {} 
+    void applyTest();
+};
+
+void TestNewDevice::applyTest()
+{
+    dev->on();
+    dev->applyLoInput();
+    dev->measureOutput();
+    dev->applyHiInput();
+    dev->measureOutput();
+    dev->off();
+}
+
+int main()
+{
+    TestDevice test_dev1{shared_ptr<DeviceInterface>(new Device)};
+    test_dev1.applyTest();
+    cout << endl;
+    TestNewDevice test_dev2{shared_ptr<NewDeviceInterface>(new NewDevice)};
+    test_dev2.applyTest();
+    cout << endl;
+    
+    return 0;
+}
+
+
